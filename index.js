8d590fc4006d60447eba07113f7dacb853fc7c53
Philippe Dionne <dionne.phil@gmail.com>
2016-12-21 17:46:59 -0500

    Add base messenger message responses

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 module.exports = function(bp) {
   bp.middlewares.load()
+
+  bp.hear({ text: /text/i, type: 'message' }, (event, next) => {
+    bp.messenger.sendText(event.user.id, 'Hello, human!')
+  })
+
+  bp.hear({ text: /attachment/i, type: 'message' }, (event, next) => {
+    bp.messenger.sendAttachment(event.user.id, 'image', 'https://dialoganalytics.com/images/og-image.png')
+  })
+
+  bp.hear({ text: /template/i, type: 'message' }, (event, next) => {
+    payload = {
+      template_type: "button",
+      text: "Check out our documentation!",
+      buttons: [
+        {
+          type: "web_url",
+          url: "https://docs.dialoganalytics.com",
+          title: "Documentation"
+        }
+      ]
+    }
+
+    bp.messenger.sendTemplate(event.user.id, payload, { typing: 2000 })
   })
 }
